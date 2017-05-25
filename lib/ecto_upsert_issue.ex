663517b94eff4d168db9ebb8744d6f7440f194cd
Sean Tan <sean.tan.ly@gmail.com>
2017-05-25 23:25:16 +0800

    initial commit

---
+++ lib/ecto_upsert_issue.ex
@@ -0,0 +1,18 @@
+defmodule EctoUpsertIssue do
+  @moduledoc """
+  Documentation for EctoUpsertIssue.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoUpsertIssue.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_upsert_issue_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoUpsertIssueTest do
+  use ExUnit.Case
+  doctest EctoUpsertIssue
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
